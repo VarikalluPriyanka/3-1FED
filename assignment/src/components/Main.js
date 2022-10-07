5861fd0dd37b043aed6fc51fd8dc6ad9f899bb65
VarikalluPriyanka <priya.varikallu@gmail.com>
2022-10-07 16:22:09 +0530

    assignment-fedblog

---
+++ assignment/src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainContent from "./Prog";
+const Main = () => {
+    return (
+
+    <div>
+
+  <div className="article">
+   <div> <MainContent /></div>
+  </div>
+     
+    <Sidebar />
+    </div>
+    )
+};
+
+export default Main;
